824990149048e7f447c3f8a9d700ae4795b6c91e
xabixab <xabixab@gmail.com>
2017-03-16 23:55:56 +0100

    arduino groundstation node receiver sample code

---
+++ arduino/gsExample/gsExample.ino/gsExample.ino.ino
@@ -0,0 +1,35 @@
+// Copyright Benoit Blanchon 2014-2017
+// MIT License
+//
+// Arduino JSON library
+// https://github.com/bblanchon/ArduinoJson
+// If you like this project, please add a star!
+
+#include <ArduinoJson.h>
+const int seed = 23;
+void setup() {
+  Serial.begin(115200);
+  randomSeed(analogRead(0) + seed);
+  while (!Serial) {
+  }
+}
+
+void loop() {
+  sendSerial("pre", random(0, 700));
+  delay(100);
+  sendSerial("temp", random(0, 35));
+  delay(100);
+  sendSerial("alt", random(400, 1200));
+  delay(100);
+}
+
+int sendSerial(String sensor, float value) {
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& receiverMessage = jsonBuffer.createObject();
+  receiverMessage["sensor"] = sensor;
+  receiverMessage["value"] = value;
+  receiverMessage.printTo(Serial);
+  Serial.println();
+  return 0;
+}
+
